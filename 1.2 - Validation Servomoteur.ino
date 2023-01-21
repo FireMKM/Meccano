3365bbd96d3507b88e5049f3560824aadfdfc525
FireDev <78695091+FireMKM@users.noreply.github.com>
2023-01-21 20:09:04 +0100

    Add files via upload

---
+++ 1.2 - Validation Servomoteur.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+
+const int PIN_SERVO = 8;
+Servo myservo;
+
+const byte GAUCHE = 47;
+const byte DROIT = 77;
+const byte DROITE = 107;
+
+
+void setup() {
+  myservo.attach(PIN_SERVO);
+}
+  
+
+void loop() {  
+  myservo.write(77);
+  delay(2000);
+  myservo.write(47);
+  delay(3000);
+  myservo.write(107);
+  delay(5000);
+}
