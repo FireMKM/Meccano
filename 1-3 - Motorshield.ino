26d803b977d1685e54e32150cf30b26c80453283
FireDev <78695091+FireMKM@users.noreply.github.com>
2023-02-14 16:33:37 +0100

    Update 1-3 - Motorshield.ino

--- 1-3 - Motorshield.ino
+++ 1-3 - Motorshield.ino
@@ -1,3 +1,5 @@
+// Programme de test du moteur
+
 // Required libraries to run this code
 // First one is the dedicated library for the Adafruit MotorShield device, and the second one is a required library to run the first
 // Adafruit MotorShield V2: https://github.com/adafruit/Adafruit_Motor_Shield_V2_Library
